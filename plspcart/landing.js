1fc68af93d308a5e926753997baeb317b11eb742
PrinceBanatlao <145571479+PrinceBanatlao@users.noreply.github.com>
2025-10-18 17:00:53 +0800

    Add files via upload

---
+++ plspcart/landing.js
@@ -0,0 +1,29 @@
+// landing.js
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.classList.add('active');
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.classList.remove('active');
+    }
+}
+
+function switchModal(closeId, openId) {
+    closeModal(closeId);
+    setTimeout(() => openModal(openId), 300);
+}
+
+// Close modal if clicking outside
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target === modals[i]) {
+            closeModal(modals[i].id);
+        }
+    }
+}
